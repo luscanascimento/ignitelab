d3eddcf12d0160f471d196908b0d532bac85506e
Lucas Nascimento <lucas.lntechlab@gmail.com>
2022-10-14 23:13:09 -0300

    feature: finalized app

--- src/App.tsx
+++ src/App.tsx
@@ -63,12 +63,12 @@ export function App() {
       </form>
       <footer className="flex flex-col items-center gap-4 mt-8"></footer>
       <Text asChild size="sm">
-        <a href="" className="text-gray-400 underline">
+        <a href="" className="text-gray-400 underline hover:text-gray-200">
           Esqueceu sua senha?
         </a>
       </Text>
       <Text asChild size="sm">
-        <a href="" className="text-gray-400 underline">
+        <a href="" className="text-gray-400 underline hover:text-gray-200">
           Não possui conta? Crie uma agora!
         </a>
       </Text>
